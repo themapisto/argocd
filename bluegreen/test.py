c589cef7f23aec646c1f8f54a8c7bb1dfabf11c6
richard <richard@megazone.com>
2022-08-16 10:37:25 +0000

    kpkim

--- bluegreen/test.py
+++ bluegreen/test.py
@@ -4,10 +4,10 @@ import subprocess
 res = subprocess.check_output('cat /home/ubuntu/argocd/argocd/bluegreen/rollout.yaml | grep "argoproj/rollouts-demo:" ', shell=True)
 reslen= len(res)
 if reslen==44 :
-    print ("green to blue")
+    print (" blue to green")
     os.system("sh /home/ubuntu/argocd/argocd/bluegreen/blue.sh")
 else :
-    print ("blue to green")
+    print ("green to blue")
     os.system("sh /home/ubuntu/argocd/argocd/bluegreen/green.sh")
 
 
--- canary/test.py
+++ canary/test.py
@@ -8,8 +8,8 @@ b=result[-4]
 print(b)
 
 if b=="1" :
-    print ("1.0 to 2.0")
+    print ("2.0 to 1.0")
     os.system("sh /home/ubuntu/argocd/argocd/canary/2.0.sh")
 else :
-    print ("2.0 to 1.0")
+    print ("1.0 to 2.0")
     os.system("sh /home/ubuntu/argocd/argocd/canary/1.0.sh")
