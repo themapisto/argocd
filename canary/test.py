3ffb137405029517a6a662355753a6cdee1e15b9
richard <richard@megazone.com>
2022-08-16 10:22:44 +0000

    kpkim

--- canary/test.py
+++ canary/test.py
@@ -9,7 +9,7 @@ print(b)
 
 if b=="1" :
     print ("1.0 to 2.0")
-    os.system("sh 2.0.sh")
+    os.system("sh /home/ubuntu/argocd/argocd/canary/2.0.sh")
 else :
     print ("2.0 to 1.0")
-    os.system("sh 1.0.sh")
+    os.system("sh /home/ubuntu/argocd/argocd/canary/1.0.sh")
