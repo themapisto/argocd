137fda63535c1b51b2617508f1e46d5e7c36dd60
richard <richard@megazone.com>
2022-08-16 10:32:42 +0000

    kpkim

--- bluegreen/test.py
+++ bluegreen/test.py
@@ -1,13 +1,13 @@
 import os
 import subprocess
 
-res = subprocess.check_output('cat /home/ubuntu/argocd/argocd/bluegreen/rollout.yaml | grep "argoproj/rollouts-demo:" ', shell=True)
+res = subprocess.check_output('cat rollout.yaml | grep "argoproj/rollouts-demo:" ', shell=True)
 reslen= len(res)
 if reslen==44 :
     print ("green to blue")
-    os.system("sh /home/ubuntu/argocd/argocd/bluegreen/blue.sh")
+    os.system("sh blue.sh")
 else :
     print ("blue to green")
-    os.system("sh /home/ubuntu/argocd/argocd/bluegreen/green.sh")
+    os.system("sh green.sh")
 
 
